44ba36817f5d98e4fe7c116c06a3e11b26baec83
nodogklaa <noahklaassen96@gmail.com>
2026-03-29 00:42:04 +1300

    Add files via upload

---
+++ FIRMWARE/noahpad/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6060
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    "Noah"
+#define PRODUCT         "NoahPad"
+
+
+#define MATRIX_ROWS     3
+#define MATRIX_COLS     4
+#define DIODE_DIRECTION COL2ROW
+
+
+#define ENCODER_RESOLUTION 4
